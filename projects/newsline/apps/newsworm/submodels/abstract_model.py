aec0dab06fc9d1f3d6cec32428c57e68a9d1feb4
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2016-04-30 13:45:36 +0100

    Fixed concrete inheritance to abstract inheritance in newsworm app

--- projects/newsline/apps/newsworm/submodels/abstract_model.py
+++ projects/newsline/apps/newsworm/submodels/abstract_model.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 class AbstractModel(models.Model):
-	abstract = True
 	class Meta:
 		app_label = "newsworm"
+		abstract = True
